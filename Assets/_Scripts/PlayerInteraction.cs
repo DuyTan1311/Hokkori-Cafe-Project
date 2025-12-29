95772d4e39ab9804b80733eaf83439bcb9c45bea
DuyTan1311 <buiduytann1311@gmail.com>
2025-12-29 19:54:27 +0700

    Set priority in PlayerInteraction
    Make player interacts with objects based on distance when collide with multiple objects

--- Assets/_Scripts/PlayerInteraction.cs
+++ Assets/_Scripts/PlayerInteraction.cs
@@ -1,34 +1,70 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    Interactable currentInteractable;
+    // declare a list to store all the interactable object in range
+    List<Interactable> interactablesInRange = new List<Interactable>();
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.TryGetComponent(out Interactable interactable))
+        // check if the collided object has interactable component
+        if (other.TryGetComponent(out Interactable interactable))
         {
-            currentInteractable = interactable;
+            // check if the list has contained that object or not
+            if (!interactablesInRange.Contains(interactable))
+            {
+                // if the collided object hasn't been added, add it to the list
+                interactablesInRange.Add(interactable);
+            }
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
-        if(other.TryGetComponent(out Interactable interactable))
+        // check if the collided object has interactable component
+        if (other.TryGetComponent(out Interactable interactable))
         {
-            if(currentInteractable == interactable)
-            {
-                currentInteractable = null;
-            }
+            // remove the object from the list
+            interactablesInRange.Remove(interactable);
         }
     }
 
     public void TryInteract()
     {
-        if(currentInteractable == null)
+        // if there's no interactable object in the list, return
+        if (interactablesInRange.Count == 0)
         {
             return;
         }
-        currentInteractable.Interact();
+        // declare a reference to store the closest interactable
+        Interactable closest = null;
+        // declare a closest distance variable as a large number for easier comparisons
+        float closestDistance = float.MaxValue;
+        // set player's current position
+        Vector3 playerPos = transform.position;
+        // check every object in the list
+        foreach (var interactable in interactablesInRange)
+        {
+            // if there's an empty slot or the object is not interactable, skip
+            if (interactable == null || !interactable.IsInteractable())
+            {
+                continue;
+            }
+            // calculate squared distance for faster performance
+            float distance = (interactable.transform.position - playerPos).sqrMagnitude;
+            // if the distance is smaller than closest distance, set it to the closest distance
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                // set the closest object
+                closest = interactable;
+            }
+        }
+        // if there's a closest interactable object, interact with it
+        if (closest != null)
+        {
+            closest.Interact();
+        }
     }
-
 }
